69b2f6ce3b1b6d566dcb059aafe93f83d1f34b73
FyVoid <mythstudiofyvoid@outlook.com>
2023-11-02 11:38:52 +0800

    added README

--- generate.py
+++ generate.py
@@ -1,5 +1,11 @@
 from pytester import tester
 from pytester import analyser
 
+gen_label_prob = 0.2
+former_addr_prob = 0.5
+total_instruct = 114
+
 tester = tester.Tester('tester_config.txt')
-tester.gen_assembly('assemble.asm', 114)
+tester.gen_label_prob = gen_label_prob
+tester.former_addr_prob = former_addr_prob
+tester.gen_assembly('assemble.asm', total_instruct=total_instruct)
