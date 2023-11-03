7de7e47417e23b36abf6a05c82cee5534994841f
FyVoid <mythstudiofyvoid@outlook.com>
2023-11-03 15:08:55 +0800

    增加了一点批量测试中的提示，让你不觉得你的计算机死掉了

--- batch_test.sh
+++ batch_test.sh
@@ -12,10 +12,14 @@ mkdir $RESULT_DIR
 
 for ((i=0; i<BATCH; i++))
     do
+    echo "test ${i}"
     dirname="${BATCH_DIR}-${i}"
     sh CO_test.sh nw nl ns dir ${dirname} > stdout.txt
     mv -f ${dirname} $RESULT_DIR
     mv -f stdout.txt "${RESULT_DIR}/${dirname}"
+    echo "test ${i} completed"
 done
 
 python batch_analyse.py $RESULT_DIR $BATCH_DIR $BATCH
+
+cat batch_log.txt
