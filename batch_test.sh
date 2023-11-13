ea1df259f06a77a1039cf4a42a32e048f502cfa6
FyVoid <mythstudiofyvoid@outlook.com>
2023-11-13 15:11:45 +0800

    支持延迟漕选项

--- batch_test.sh
+++ batch_test.sh
@@ -1,11 +1,20 @@
 BATCH=50
 BATCH_DIR="result"
 RESULT_DIR="test_batch"
+DELAY_SLOT="false"
 
 if [ $# -gt 0 ]
     then
     BATCH=$1
 fi
+
+for arg in $*
+    do
+    if [ $arg = "ds" ]
+        then
+        DELAY_SLOT="true"
+    fi
+done
 
 rm -rf $RESULT_DIR
 mkdir $RESULT_DIR
@@ -14,7 +23,12 @@ for ((i=0; i<BATCH; i++))
     do
     echo "test ${i}"
     dirname="${BATCH_DIR}-${i}"
-    sh CO_test.sh nw nl ns sf dir ${dirname} > stdout.txt
+    if [ $DELAY_SLOT = "false" ]
+        then
+        sh CO_test.sh nw nl ns sf dir ${dirname} > stdout.txt
+    else
+        sh CO_test.sh nw nl ns ds sf dir ${dirname} > stdout.txt
+    fi
     mv -f ${dirname} $RESULT_DIR
     mv -f stdout.txt "${RESULT_DIR}/${dirname}"
     echo "completed"
